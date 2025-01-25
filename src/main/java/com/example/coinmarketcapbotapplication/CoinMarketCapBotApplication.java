a8043ad2c4e59466c2221a0d09a5b6ba4738b834
black32h <blackspetya@gmail.com>
2025-01-25 15:13:38 +0200

    Update CoinMarketCapBotApplication.java

--- src/main/java/com/example/coinmarketcapbotapplication/CoinMarketCapBotApplication.java
+++ src/main/java/com/example/coinmarketcapbotapplication/CoinMarketCapBotApplication.java
@@ -5,15 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@SpringBootApplication
-@EnableScheduling
+// Основний клас програми для запуску Spring Boot застосунку
+@SpringBootApplication  // Вказує, що цей клас є точкою входу для Spring Boot застосунку
+@EnableScheduling       // Дозволяє використання планувальника задач (для регулярного виконання задач)
 public class CoinMarketCapBotApplication {
+
+    // Головний метод для запуску програми
     public static void main(String[] args) {
+        // Запуск Spring Boot застосунку
         SpringApplication.run(CoinMarketCapBotApplication.class, args);
     }
 }
-
 
 
 
