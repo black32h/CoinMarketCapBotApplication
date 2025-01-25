62b5844427511ffdb00cfcbf62e49df29d067ff6
black32h <blackspetya@gmail.com>
2025-01-25 15:16:52 +0200

    Update CustomResponseParameters.java

--- src/main/java/com/example/coinmarketcapbotapplication/CustomResponseParameters.java
+++ src/main/java/com/example/coinmarketcapbotapplication/CustomResponseParameters.java
@@ -1,4 +1,11 @@
 package com.example.coinmarketcapbotapplication;
 
+import lombok.Getter;
+import lombok.Setter;
+
+// Клас для зберігання параметрів відповіді, зокрема часу затримки при помилці 429
+@Getter
+@Setter
 public class CustomResponseParameters {
+    private String retryAfter;  // Час затримки у секундах, після якого можна повторити запит
 }
