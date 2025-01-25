c1a4fa0667b1a1f2ef88d17086522ceb46110337
black32h <blackspetya@gmail.com>
2025-01-25 15:17:31 +0200

    Update TelegramBotConfig.java

--- src/main/java/com/example/coinmarketcapbotapplication/TelegramBotConfig.java
+++ src/main/java/com/example/coinmarketcapbotapplication/TelegramBotConfig.java
@@ -1,4 +1,27 @@
 package com.example.coinmarketcapbotapplication;
 
+import com.example.coinmarketcapbotapplication.CoinMarketCapTelegramBot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
 public class TelegramBotConfig {
+
+    // Бін для створення і налаштування Telegram бота
+    @Bean
+    public CoinMarketCapTelegramBot coinMarketCapTelegramBot() {
+        return new CoinMarketCapTelegramBot();
+    }
+
+    // Бін для налаштування TelegramBotsApi та реєстрації бота
+    @Bean
+    public TelegramBotsApi telegramBotsApi(CoinMarketCapTelegramBot bot) throws TelegramApiException {
+        // Створення нового екземпляра TelegramBotsApi для реєстрації бота
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);  // Реєстрація бота у API
+        return botsApi;
+    }
 }
